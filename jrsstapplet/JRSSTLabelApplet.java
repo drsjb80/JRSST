092c2f581dafc2506f9eeb757e9022706b5e96a0
drsjb80 <drsjb80@gmail.com>
2020-02-02 11:46:57 -0700

    initial

---
+++ jrsstapplet/JRSSTLabelApplet.java
@@ -0,0 +1,35 @@
+package edu.mscd.cs.jrsstlabel;
+
+import javax.swing.JApplet;
+import java.awt.BorderLayout;
+import java.util.Arrays;
+import java.util.Vector;
+
+// http://www.realapplets.com/tutorial/ThreadExample.html
+
+public class JRSSTLabelApplet extends JApplet implements Runnable
+{
+    private JRSSTLabel label;
+
+    public void init()
+    {
+	String args = getParameter ("args");
+
+	if (args != null)
+	{
+	    args = args.trim().replaceAll ("\\s+", " ");
+	 // System.err.println (new Vector (Arrays.asList (args.split (" "))));
+	    label = new JRSSTLabel (args.split (" "));
+	}
+	else
+	{
+	    label = new JRSSTLabel (new String[]{});
+	}
+
+	setLayout (new BorderLayout());
+	add (label, "Center");
+	new Thread (this).start();
+    }
+
+    public void run() { label.doit(); }
+}
